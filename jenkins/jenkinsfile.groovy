0330fe05aece12fb527e9d7b6726f1337ef4115f
OmerGaliko <omergaliko22@gmail.com>
2021-01-02 19:13:01 +0200

    jenkins.groovy

--- jenkins/jenkinsfile.groovy
+++ jenkins/jenkinsfile.groovy
@@ -62,32 +62,4 @@ pipeline {
 
 
 
-//def userInput
-//
-//pipeline {
-//
-//    label docker-node
-//
-//    stages {
-//        stage('Input') {
-//            steps {
-//                script {
-//                    userInput = input message: 'Please provide your input', ok: 'confirm', parameters: [choice(name: '', choices: ['option 1', 'option2'], description: '')]
-//                }
-//            }
-//        }
-//        stage('Hello') {
-//            steps {
-//                dir('CoolNewDirectory') {
-//                    git branch: 'main', credentialsId: 'github_cred', url: 'https://github.com/yuribernstein2/intservice.git'
-//                    echo 'Hello World'
-//                }
-//            }
-//        }
-//        stage('Print Inputed string') {
-//            steps {
-//                println("Input was " + userInput)
-//            }
-//        }
-//    }
-//}
+
